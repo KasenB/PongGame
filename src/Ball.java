cabb3d77bcae631d16b5149d9f345fc24227c447
kasenbookbinder <kasen_bookbiner26@milton.edu>
2024-02-27 14:20:47 -0500

    worked on Ball.java

--- src/Ball.java
+++ src/Ball.java
@@ -1,4 +1,19 @@
 import java.awt.*;
 
 public class Ball {
+    public String name;
+    public int xPos;
+    public int yPos;
+    public int dy = 5; // speed in the y direction
+    public int height = 100;
+    public int width = 75;
+    public boolean isAlive;
+    public Rectangle rec;
+
+    public Ball(String paramName, int paramXPos, int paramYPos){
+        name = paramName;
+        xPos = paramXPos;
+        yPos = paramYPos;
+        rec = new Rectangle(xPos, yPos, width, height);
+    }
 }
